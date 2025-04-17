340c874d4b6d690813f4d1820c8408bf035e0556
Steve Gelman <sgelman@1worldsync.com>
2025-04-17 16:13:20 -0500

    Added Lombok and better logging.

--- src/main/java/com/powerrviews/hs/hikariexample/controller/UserController.java
+++ src/main/java/com/powerrviews/hs/hikariexample/controller/UserController.java
@@ -2,10 +2,11 @@ package com.powerrviews.hs.hikariexample.controller;
 
 import com.powerrviews.hs.hikariexample.entity.User;
 import com.powerrviews.hs.hikariexample.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -18,11 +19,13 @@ public class UserController {
 
     @GetMapping
     public List<User> getAllUsers() {
+        log.info("Get all users");
         return userService.getAllUsers();
     }
 
     @PostMapping
     public User saveUser(@RequestBody User user) {
+        log.info("Save user: {}", user);
         return userService.saveUser(user);
     }
 }
